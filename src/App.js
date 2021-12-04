d3b83be6949e433d8cf3cba744d5e6a558001a5b
Ludvig Wärnberg Gerdin <ludvigwarnberggerdin@eduroam-2-007.epfl.ch>
2021-12-04 14:25:15 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
 import './App.css';
+
+/* import {
+ *     Navbar,
+ *     Nav,
+ *     Container
+ * } from 'react-bootstrap';
+ *  */
+import {Link} from 'react-router-dom';
 
 function App() {
     return (
 	<div className="App">
+	  {/* <Navbar className="App-navbar" expand="lg">
+	      <Container>
+	      <Navbar.Brand href="#home">
+	      Thoughts
+	      </Navbar.Brand>
+	      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+	      <Navbar.Collapse id="basic-navbar-nav">
+	      <Nav className="me-auto">
+	      </Nav>
+	      </Navbar.Collapse>
+	      </Container>
+	      </Navbar> */}
 	  <header className="App-header">
 	    <p>
 	      <span className="emoji">&#129300;</span>
@@ -11,9 +30,12 @@ function App() {
             <p>
 	      Thoughts and Notes
             </p>
+            <Link to="/home">Home</Link>
 	  </header>
 	</div>
     );
 }
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import reportWebVitals from './reportWebVitals'
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+	<Routes>
+	  <Route path="/" element={<App />} />
+	  <Route path="home" element={<Home />} />
+	</Routes>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
