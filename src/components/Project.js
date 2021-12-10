ae6f81a2a4b5a8dd726287d884475f64d25f1402
Ludvig Wärnberg Gerdin <ludvigwarnberggerdin@eduroam-6-146.epfl.ch>
2021-12-10 16:06:43 +0100

    Add Project component

---
+++ src/components/Project.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import {
+    Card,
+    ListGroup,
+    ListGroupItem
+} from 'react-bootstrap';
+
+import { AiOutlineGithub } from 'react-icons/ai';
+import { SiPubmed } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+
+function ProjectCard(props) {
+    const takeawayItems = props.keyTakeaways.map((kt) => {
+	return <ListGroupItem>{kt}</ListGroupItem>
+    })
+    
+    return (
+	<Card
+	    className="shadow-lg p-3 mb-5 bg-white rounded"
+	    style={{ width: '20rem' }}
+	>
+	  <Card.Img variant="top" src={props.pic} fluid/>
+	  <Card.Body>
+	    <Card.Title>{props.title}</Card.Title>
+	    <Card.Text>
+	      {props.summary}
+	    </Card.Text>
+	    {/* <Card.Title>Key Takeaways</Card.Title> */}
+	    {/* <ListGroup className="list-group-flush">
+		{takeawayItems}
+		</ListGroup> */}
+	    <Card.Body>
+	    </Card.Body>
+	    <Card.Link href={props.githubLink} >
+	      {props.githubLink ? <AiOutlineGithub size={50} /> : ""}
+	    </Card.Link>
+	    <Card.Link href={props.pubmedLink}>
+	      {props.pubmedLink ? <SiPubmed size={50} /> : ""}
+	    </Card.Link>
+	    <Card.Link href={props.websiteLink}>
+	      {props.websiteLink ? <BsGlobe size={50} /> : ""}
+	    </Card.Link>
+	  </Card.Body>
+	</Card>
+    );
+}
+
+export default ProjectCard;
